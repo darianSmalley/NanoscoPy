c7062c7cba256e71f2e9be4e7ae47b2b680d4f12
Darian Smalley <darian.smalley@gmail.com>
2021-09-10 02:02:48 -0400

    Changed structure to allow easier import use.

---
+++ nanoscopy/__init__.py
@@ -0,0 +1,12 @@
+"""
+NanoscoPy
+=========
+
+Import all submodules
+"""
+from . import afm
+from . import classifier
+from . import loaders
+from . import plot
+from . import raman
+from . import spm
---
+++ nanoscopy/classifier/__init__.py
@@ -0,0 +1,3 @@
+from . import datasets
+from . import evaluate
+from . import models
