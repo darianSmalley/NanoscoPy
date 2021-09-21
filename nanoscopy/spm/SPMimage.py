6ce391e7b480d37de784835120f07b88ad79bb5b
IshigamiNMDR <darianSmalley@users.noreply.github.com>
2021-09-21 19:00:17 -0400

    updated spm.io
    Updated spm.io to accommodate different traces between mtrx and sxm.

--- nanoscopy/spm/SPMimage.py
+++ nanoscopy/spm/SPMimage.py
@@ -1,18 +1,45 @@
+from collections.abc import MutableMapping
+
 CHANNEL_ERROR = 'Channel not supported.'
 DIRECTION_ERROR = 'Direction not supported.'
 
-class SPMImage:
-    traces = ['forward/up', 'forward/down', 'backward/up', 'backward/down']
-    channels = ['Z' , 'Current', 'Amplitude']
-
+class SPMImage(MutableMapping):
     def __init__(self, path='', *args, **kwargs):
         self.path = path
         self.params = dict()
         self.data = dict()
+        self.traces = dict()
+        self.update(*args, **kwargs)
     
-    def add_data(self, data, channel_name = 'Z', trace = 'forward/up'):
-        if trace not in traces: 
-            raise ValueError(DIRECTION_ERROR)
+    def add_data(self, channel, data):
+        self.data[channel].append(data)
+        
+    def add_trace(self, channel, direction, trace):
+        self.traces[channel].append({'direction': direction, 'trace': trace})
 
-        self.data[channel_name].append({trace: data})
-    
+    def add_param(self, param, value):
+        self.params[param] = value
+
+    def get_data(self, channel):
+        return zip(self.traces[channel], self.data[channel])
+
+    # The next five methods are requirements of the collection
+    def __setitem__(self, key, value):
+        self.add_data(key, value)
+    def __getitem__(self, key):
+        return self.data[key]
+    def __delitem__(self, key):
+        del self.data[key]
+    def __iter__(self):
+        return iter(self.data)
+    def __len__(self):
+        return len(self.data)
+
+    # The final two methods aren't required, but nice for demo purposes:
+    def __str__(self):
+        '''returns simple dict representation of the mapping'''
+        return str(self.data)
+    def __repr__(self):
+        '''echoes class, id, & reproducible representation in the REPL'''
+        return '{}, D({})'.format(super(SPMImage, self).__repr__(), 
+                                  self.data)        
--- nanoscopy/spm/io.py
+++ nanoscopy/spm/io.py
@@ -6,9 +6,11 @@ import pySPM
 import access2thematrix
 import matplotlib.pyplot as plt
 from pathlib import Path
-from . import SPMimage
+from .SPMImage import SPMImage
 
 spm_ext = tuple(['.sxm', '.Z_mtrx'])
+CHANNELS = ['Z' , 'Current']
+TRACES = ['forward', 'backward']
 FILES_NOT_FOUND_ERROR = "No files found."
 FILETYPE_ERROR = "SUPPORTED FILETYPE NOT FOUND. Only sxm and Z_mtrx are supported."
 
@@ -24,32 +26,38 @@ def read_sxm(path):
         data: numpy array. Contains the imported data. Most of the src_formats also ensure that the data is sorted such that the independent variable is is ascending order.
     """
     try:
+        print(f'Processing {path}...', end="", flush=True)
         # Open SXM file
         sxm = pySPM.SXM(path)
 
         # Create SPMImage class object
-        image = SPMimage(path)
+        image = SPMImage(path)
 
         # Add records of important scan parameters
         direction = sxm.header['SCAN_DIR'][0][0]
-        image.params['bias'] = float(sxm.header['Bias>Bias (V)'][0][0])
-        image.params['setpoint_value'] = float(sxm.header['Z-CONTROLLER'][1][3])
-        image.params['setpoint_unit'] = sxm.header['Z-CONTROLLER'][1][4]
-        image.params['direction'] = direction
-        image.params['width'] = sxm.size['real']['x']
-        image.params['height'] = sxm.size['real']['y']
+        bias = float(sxm.header['Bias>Bias (V)'][0][0])
+        setpoint_value = float(sxm.header['Z-CONTROLLER'][1][3])
+        setpoint_unit = sxm.header['Z-CONTROLLER'][1][4]
+        width = sxm.size['real']['x']
+        height = sxm.size['real']['y']
+
+        image.add_param('bias', bias)
+        image.add_param('setpoint_value', setpoint_value)
+        image.add_param('setpoint_unit', setpoint_unit) 
+        image.add_param('width', width)
+        image.add_param('height', height)
 
         # Get data and store in SPMImage class
-        for channel in ['Z' , 'Current']:
-            im_forward = sxm.get_channel(channel , trace = 'forward').pixels
-            im_backward = sxm.get_channel(channel , trace = 'backward').pixels
-
-            image.add_data(im_forward , channel , f'forward/{direction}')
-            image.add_data(im_backward , channel , f'backward/{direction}')
+        for channel in CHANNELS:
+            for trace in TRACES:
+                data = sxm.get_channel(channel, trace = trace).pixels
+                data = data[~np.isnan(data)]
+                image.add_data(channel, data)
+                image.add_trace(channel, direction, trace)
 
         # Close the sxm file
         sxm.closefile() # Note: This method is from our modified version of SXM
-        
+        print('DONE')
         return image
 
     except Exception as error:
@@ -68,11 +76,17 @@ def read_mtrx(path):
         data: numpy array. Contains the imported data. Most of the src_formats also ensure that the data is sorted such that the independent variable is is ascending order.
     """
     try: 
-        mtrx = access2thematrix.MtrxData()
-        traces, message = mtrx.open(path)
+        print(f'Processing {path}...', end="", flush=True)
+        # Get I and Z mtrx file paths for this image.      
+        mtrxs_ZI = {
+            'Z': glob.glob(os.path.join(path, '*.Z_mtrx')),
+            'Current': glob.glob(os.path.join(path, '*.I_mtrx'))
+        }
 
-         # Create SPMImage class object
-        image = SPMimage(path)
+        # Create SPMImage class object
+        image = SPMImage(path)
+        # Create mtrx class to access data
+        mtrx = access2thematrix.MtrxData()
 
         # Retreive records of important scan parameters
         setpoint, setpoint_unit = mtrx.param['EEPA::Regulator.Setpoint_1']
@@ -81,31 +95,35 @@ def read_mtrx(path):
         scan_height, _ = mtrx.param['EEPA::XYScanner.Height']
 
         # Add important scan parameters to SPMimage
-        image.params['setpoint_value'] = setpoint
-        image.params['setpoint_unit'] = setpoint_unit
-        image.params['bias'] = voltage
-        image.params['width'] = scan_width
-        image.params['height'] = scan_height
+        image.add_param('setpoint_value', setpoint)
+        image.add_param('setpoint_unit', setpoint_unit)
+        image.add_param('bias', voltage)
+        image.add_param('width', scan_width)
+        image.add_param('height', scan_height)
 
         # Get data and store in SPMImage class
-        for trace in traces:
-            trace_image, message = mtrx.select_image(trace)
-            trace_data = trace_image.data[~np.isnan(image.data)]
-            image.add_data(trace_data , 'Z', trace)
+        for channel, mtrx_path in mtrxs_ZI:
+            rasters, message = mtrx.open(mtrx_path)
+            for raster in rasters:
+                data, message = mtrx.select_image(raster).data
+                data = data[~np.isnan(data)]
+                image.add_data(channel, data)
+                
+                direction, trace = raster.split('/')
+                image.add_trace(channel, direction, trace)
 
+        print('DONE')
         return image
 
     except Exception as error:
-        print(message)
         raise
 
-def read(path, filter = ''):
+def read(path):
     """
     Imports tabular data from a text file.
 
     Inputs:
         path: A path-like object representing a file system path. A path-like object is either a string or bytes object representing a path.
-        filter: A string which specifies which files should be read.
     
     Outputs:
         data: DataFrame. Contains the imported data. Most of the src_formats also ensure that the data is sorted such that the independent variable is is ascending order.
@@ -113,27 +131,36 @@ def read(path, filter = ''):
     # Check if path leads to a file or a folder
     if os.path.isfile(path):
         # If file, add to list as single element
-        paths = [path]          
+        paths = [path]
     else:
-        # if path points to a folder, 
-        # get all paths in folder according to filter
-        paths = glob.glob(os.path.join(path, f"*{filter}*"))
+        # if path points to a folder, get all paths in folder 
+        paths = glob.glob(os.path.join(path, '*'), recursive=True)
 
     # Check if any files were found
     if len(paths) == 0:
         raise ValueError(FILES_NOT_FOUND_ERROR)
+
+    images = []
     
-    # read files with appropraite import function
-    for path in paths: 
-        if path.endswith(".sxm"):
-            images = list(map(read_sxm, paths))
-        elif path.endswith(".Z_mtrx"):
-            images = list(map(read_mtrx, paths))
-        else:
-            raise ValueError(FILETYPE_ERROR)           
+    # Get all files with sxm extension
+    sxms = list(filter(lambda p: p.endswith('sxm'), paths))
+    sxm_images = list(map(read_sxm, sxms))
+    images.extend(sxm_images)
+
+    # Get all files with mtrx extension
+    mtrxs = list(filter(lambda p: p.endswith('mtrx'), paths))
+    # convert list of absolute paths to list of file names 
+    mtrxs = list(map(lambda path: Path(path).stem, mtrxs))
+    # make dictionary from list to obtain unique file names, 
+    mtrxs = set(mtrxs)
+    for file_name in mtrxs:
+        # Combine Z and I mtrx files into single SPMimages
+        partial_path = os.path.join(path, file_name)
+        mtrx_images = read_mtrx(partial_path)
+        images.extend(mtrx_images)
 
     return images
-    
+
 def export_spm(images, dst_paths = ['./'], ext = 'jpeg', 
                scan_dir = 'up', cmap = 'gray'):
     """
