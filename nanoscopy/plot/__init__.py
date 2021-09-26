413663f3a7d58ccc2b2d22f28548224fd4d647d9
Jesse Thompson <64974444+Jesset3@users.noreply.github.com>
2021-09-26 15:53:26 -0400

    Updated plotting functions
    Added functionality for packaging and using custom matplotlib stylesheets without affecting matplotlib folder. Also updated PlotGen to use stylesheets where possible.

--- nanoscopy/plot/__init__.py
+++ nanoscopy/plot/__init__.py
@@ -1,1 +1,2 @@
 from . import PlotGen
+from . import plot_styles
---
+++ nanoscopy/plot/plot_styles.py
@@ -0,0 +1,6 @@
+import os
+
+# Get full directory path for the plot folder.
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+style_dict = {'jetplot': os.path.join(dir_path, 'matplotlib_styles\jetplot.mplstyle')}
