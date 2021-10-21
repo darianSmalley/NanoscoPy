70bc7d0dc8066648f9417a47c400b89a8a11541f
Jesse Thompson <64974444+Jesset3@users.noreply.github.com>
2021-10-21 00:54:45 -0400

    Separated some font size setting from original stylesheet

--- nanoscopy/plot/plot_styles.py
+++ nanoscopy/plot/plot_styles.py
@@ -3,4 +3,4 @@ import os
 # Get full directory path for the plot folder.
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-style_dict = {'jetplot': os.path.join(dir_path, 'matplotlib_styles\jetplot.mplstyle')}
+style_dict = {'jetplot': os.path.join(dir_path, 'matplotlib_styles\jetplot.mplstyle') , 'jet-paper': os.path.join(dir_path, 'matplotlib_styles\jet-paper.mplstyle')}
