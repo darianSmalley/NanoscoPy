f8b6954b02bcb74507bcfdd06cbaba6041a477bd
Jesse Thompson <64974444+Jesset3@users.noreply.github.com>
2021-11-05 00:16:03 -0400

    Update some plotting functions to better handle stylesheets

--- nanoscopy/plot/plot_styles.py
+++ nanoscopy/plot/plot_styles.py
@@ -3,5 +3,4 @@ import os
 # Get full directory path for the plot folder.
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# Generate a dictionary containing the paths to custom Matplotlib stylesheets. This is used for importing the stylesheets into Matplotlib
-style_dict = {'jetplot': os.path.join(dir_path, 'matplotlib_styles\jetplot.mplstyle')}
+style_dict = {'jetplot': os.path.join(dir_path, 'matplotlib_styles\jetplot.mplstyle') , 'jet-paper': os.path.join(dir_path, 'matplotlib_styles\jet-paper.mplstyle')}
