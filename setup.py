9b89f41a3bf64c58ebcfe04dedecd5bbd7003d42
Darian Smalley <darian.smalley@gmail.com>
2021-12-07 20:59:29 -0500

    Added files for conda-forge distribution

--- setup.py
+++ setup.py
@@ -1,3 +1,37 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='NanoscoPy',
+    version='0.0.7',
+    author = "Jesse Thompson, Darian Smalley",
+    author_email = "darian.smalley@gmail.com",
+    description = "microscopy data processor",
+    long_description = readme(),
+    url = 'https://github.com/darianSmalley/NanoscoPy',
+    packages= find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Microscopy'
+                ],
+    install_requires=[
+        'numpy', 
+        'pandas', 
+        'sklearn', 
+        "SPIEPy", 
+        "scipy",
+        "matplotlib",
+        "access2theMatrix",
+        "lmfit",
+        "pySPM",
+        "SPM",
+        "wheel"
+        ],
+    python_requires='>=3.6'
+)
