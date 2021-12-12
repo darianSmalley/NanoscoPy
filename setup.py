64b0819a6ef6cb87717b997b7735c66bb51c4413
Darian Smalley <darian.smalley@gmail.com>
2021-12-12 16:00:33 -0500

    Still trying to get auto dependency installation working.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='NanoscoPy',
-    version='0.0.7',
+    version='0.0.13',
     author = "Jesse Thompson, Darian Smalley",
     author_email = "darian.smalley@gmail.com",
     description = "microscopy data processor",
@@ -17,21 +17,17 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Scientific/Engineering :: Microscopy'
+        'License :: OSI Approved :: MIT License'
                 ],
     install_requires=[
-        'numpy', 
-        'pandas', 
-        'sklearn', 
-        "SPIEPy", 
-        "scipy",
-        "matplotlib",
-        "access2theMatrix",
-        "lmfit",
-        "pySPM",
-        "SPM",
-        "wheel"
+        'numpy >= 1.20.3', 
+        'pandas >= 1.3.1', 
+        "SPIEPy >= 0.2.0", 
+        "scipy >= 1.7.1",
+        "matplotlib >= 3.4.2",
+        "access2theMatrix >= 0.4.1",
+        "lmfit >= 1.0.2",
+        "pySPM >= 0.2.20"
         ],
     python_requires='>=3.6'
 )
